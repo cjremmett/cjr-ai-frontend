7f5e52b78314614c6f52846a2790270ea806eecc
cjremmett <cjremmett@gmail.com>
2025-06-04 15:50:03 -0400

    some more css fixes

--- components/Chatbot/GoogleSignIn.js
+++ components/Chatbot/GoogleSignIn.js
@@ -5,13 +5,17 @@ import './GoogleSignIn.css';
 function GoogleSignIn({ login, logOut, profile }) {
   return (
     <div className="googleSignInContainer">
+      <div className="welcome-area">
+        {profile && (
+          <h3 style={{ margin: 0, fontSize: '1rem', fontWeight: 400 }}>
+            Welcome, {profile.name}!
+          </h3>
+        )}
+      </div>
       {!profile ? (
         <button onClick={login}>Sign in with Google</button>
       ) : (
-        <div style={{ width: '100%' }}>
-          <h3 style={{ margin: 0, fontSize: '1rem', fontWeight: 400 }}>Welcome, {profile.name}!</h3>
-          <button onClick={logOut}>Sign Out</button>
-        </div>
+        <button onClick={logOut}>Sign Out</button>
       )}
     </div>
   );
