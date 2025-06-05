7042e2a4800ac81b2148d3ec68fdf37165aa7cea
cjremmett <cjremmett@gmail.com>
2025-06-05 07:41:30 -0400

    adjust css, remove text from google sign in component

--- components/Chatbot/GoogleSignIn.js
+++ components/Chatbot/GoogleSignIn.js
@@ -5,13 +5,6 @@ import './GoogleSignIn.css';
 function GoogleSignIn({ login, logOut, profile }) {
   return (
     <div className="googleSignInContainer">
-      <div className="welcome-area">
-        {profile && (
-          <h3 style={{ margin: 0, fontSize: '1rem', fontWeight: 400 }}>
-            Welcome, {profile.name}!
-          </h3>
-        )}
-      </div>
       {!profile ? (
         <button onClick={login}>Sign in with Google</button>
       ) : (
